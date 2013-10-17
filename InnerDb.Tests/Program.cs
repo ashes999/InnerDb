2dd5dc9dd71d9c5f827d7ee2201fea65ad631beb
ashes999 <ashes999@yahoo.com>
2013-10-16 23:38:21 -0400

    Beefed up unit test project
    It's an EXE that runs NUnit. But you can still run the EXE through
    NUnit's GUI console.

---
+++ InnerDb.Tests/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace InnerDb.Tests
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            string nUnitPath = System.Configuration.ConfigurationManager.AppSettings["NUnitPath"];
+            if (string.IsNullOrEmpty(nUnitPath)) {
+                Console.WriteLine("The key 'NUnitPath' is missing from App.config. Tell it where nunit-console.exe is (including the EXE name).");
+            } else {
+                Console.WriteLine(string.Format("Running NUnit ({0}) ...\n", nUnitPath));
+                
+                Process p = new Process();
+                p.StartInfo = new ProcessStartInfo(nUnitPath, Assembly.GetExecutingAssembly().Location)
+                {
+                    UseShellExecute = false
+                };
+                p.Start();
+                p.WaitForExit();
+            }
+            Console.WriteLine("\nPress any key to end.");
+            Console.ReadKey();
+        }
+    }
+}
